85d3c7d977d8bd3bfa7ca0b66fb5f2b116ff950b
Sam Bossley <bossley.samuel@gmail.com>
2023-04-29 12:35:03 -0700

    fix: suppress unedited image variants

--- src/photos/convertAlbum.ts
+++ src/photos/convertAlbum.ts
@@ -1,3 +1,4 @@
+import { assert } from 'std/testing/asserts.ts'
 import { Slugify } from '@/utils/slugify.ts'
 
 type ImageMetadata = {
@@ -15,18 +16,17 @@ export async function ConvertAlbum(albumDir: string, outputDir: string) {
   )
 
   for (const fileEntry of Deno.readDirSync(albumDir)) {
-    if (!fileEntry.isFile || fileEntry.name.endsWith('.json')) {
+    if (
+      !fileEntry.isFile || fileEntry.name.endsWith('.json') ||
+      await hasEditedVariant(albumDir + '/' + fileEntry.name)
+    ) {
       continue
     }
 
+    const inFile = albumDir + '/' + fileEntry.name
+
     try {
-      // TODO parse edited files and suppress unedited variants
-      const metaFilename = fileEntry.name + '.json'
-      const metadataFile = albumDir + '/' + metaFilename
-      await Deno.stat(metadataFile)
-      const metadata: ImageMetadata = JSON.parse(
-        await Deno.readTextFile(metadataFile),
-      )
+      const metadata = await getMetadata(inFile)
 
       const timestamp = metadata?.photoTakenTime?.formatted
       if (!timestamp) {
@@ -50,23 +50,52 @@ export async function ConvertAlbum(albumDir: string, outputDir: string) {
         fileExt = 'jpg'
       }
 
-      const inFile = albumDir + '/' + fileEntry.name
       const outFile = dateDir + '/' + formattedDate + '.' + fileExt
 
+      // TODO suppress collisions
       Deno.copyFile(inFile, outFile)
     } catch {
       // metadata does not exist for image
       await Deno.mkdir(unknownDir, { recursive: true })
 
-      const formattedAlbumName = Slugify(albumName)
-      if (!formattedAlbumName) {
-        console.log('invalid formatted album name. Comes from', albumName)
-      } else {
-        Deno.copyFile(
-          albumDir + '/' + fileEntry.name,
-          unknownDir + '/' + Slugify(albumName) + '-' + fileEntry.name,
-        )
-      }
+      // TODO suppress collisions
+      Deno.copyFile(
+        albumDir + '/' + fileEntry.name,
+        unknownDir + '/' + Slugify(albumName) + '-' +
+          fileEntry.name.replace(/\s+/g, '-'),
+      )
     }
+  }
+}
+
+async function hasEditedVariant(file: string) {
+  const editedFile = file.replace(/\..+$/, '-edited$&')
+
+  try {
+    const finfo = await Deno.stat(editedFile)
+    assert(finfo.isFile)
+    return true
+  } catch {
+    return false
+  }
+}
+
+async function getMetadata(file: string): Promise<ImageMetadata> {
+  try {
+    const metadataFile = file + '.json'
+    await Deno.stat(metadataFile)
+    return JSON.parse(
+      await Deno.readTextFile(metadataFile),
+    )
+  } catch {
+    if (!file.includes('-edited.')) {
+      throw new Error('no metadata found.')
+    }
+    const uneditedMetadataFile = file.replace(/-edited\./, '.') + '.json'
+
+    await Deno.stat(uneditedMetadataFile)
+    return JSON.parse(
+      await Deno.readTextFile(uneditedMetadataFile),
+    )
   }
 }
