0c75b2bfde91dfc345b5e917b90908d63d2b8974
Sam Bossley <bossley.samuel@gmail.com>
2023-04-29 14:44:09 -0700

    fix: remove duplicate album images

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -2,6 +2,12 @@ export async function CopyAvoidCollisions(inFile: string, outFile: string) {
   try {
     await Deno.stat(outFile)
 
+    // if images are found in multiple albums, they are duplicated
+    // so we can ignore files that already exist byte for byte
+    if (await areFilesIdentical(inFile, outFile)) {
+      return
+    }
+
     const fileExt = outFile.replace(/^.+\./, '')
     const filePrefix = outFile.substring(0, outFile.length - fileExt.length - 1)
 
@@ -31,4 +37,30 @@ export async function CopyAvoidCollisions(inFile: string, outFile: string) {
       outFile,
     )
   }
+}
+
+async function areFilesIdentical(
+  filePath1: string,
+  filePath2: string,
+): Promise<boolean> {
+  const [file1, file2] = await Promise.all([
+    Deno.readFile(filePath1),
+    Deno.readFile(filePath2),
+  ])
+
+  if (file1.length !== file2.length) {
+    return false
+  }
+
+  let isIdentical = true
+  let i = 0
+  while (i < file1.length) {
+    if (file1[i] !== file2[i]) {
+      isIdentical = false
+      break
+    }
+    i++
+  }
+
+  return isIdentical
 }
